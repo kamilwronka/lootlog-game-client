26195d7b30f67bdc504fa3802abc37eca1900f0c
Kamil Wronka <kamil.wronka@ext.allfunds.com>
2024-10-09 13:53:35 +0200

    feat: fix entrypoint script

--- src/scripts/copy-entrypoint.sh
+++ src/scripts/copy-entrypoint.sh
@@ -5,7 +5,9 @@ env=${ENVIRONMENT:-$default_env}
 
 echo "Using environment: $env";
 
-cp src/templates/entrypoint.js dist/entrypoint.js
+cp src/templates/entrypoint.js dist/entrypoint.js;
+
+ls dist;
 
 sed -i '' '23i\
 const env = "'$env'";
